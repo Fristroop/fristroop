4199e748e57c71caff3ff32fed5762543664914d
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2023-10-22 20:27:19 +0300

    theme changer updated

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
 export const Theme = () => {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState(document.body.getAttribute("data-bs-theme"));
   const changeTheme = () => {
     const target = theme == "dark" ? "light" : "dark";
     document.body.setAttribute("data-bs-theme", target);
+    localStorage.setItem("theme", target);
     setTheme(target);
   };
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { App } from "./Pages/App";
 import { NotFound } from "./Pages/NotFound";
 
-document.body.setAttribute("data-bs-theme", "dark")
+document.body.setAttribute(
+  "data-bs-theme",
+  localStorage.getItem("theme") || "dark"
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
